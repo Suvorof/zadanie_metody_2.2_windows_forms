669c2f9b2d3edbb4669800e992d82fd615330b72
Konstantin Suvorov <konsu@inbox.ru>
2018-05-09 19:02:25 +0300

    Добавьте файлы проекта.

---
+++ zadanie_metody_2.2_windows_forms/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadanie_metody_2._2_windows_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(this.textBox1.Text);
+            double b = Convert.ToDouble(this.textBox2.Text);
+
+            if (radioButton1.Checked)
+            {
+                this.textBox3.Text = (a % b).ToString();
+            }
+
+            if (radioButton2.Checked)
+            {
+                this.textBox3.Text = (Math.Pow(a, b)).ToString();
+            }
+
+            if (radioButton3.Checked)
+            {
+                this.textBox3.Text = (a + b).ToString();
+            }
+
+            if (radioButton4.Checked)
+            {
+                this.textBox3.Text = (a / b).ToString();
+            }
+        }
+    }
+}
---
+++ zadanie_metody_2.2_windows_forms/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("zadanie_metody_2.2_windows_forms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("zadanie_metody_2.2_windows_forms")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ab3c88d-c649-4390-8047-0c58645afc04")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
